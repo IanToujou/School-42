4f941b22c54153152572143238535c37d4a6e8e2
ibour <ianbour2016@gmail.com>
2024-07-11 19:33:41 +0200

    Worked on ft_rev_int_tab.c

--- C-01/ex07/ft_rev_int_tab.c
+++ C-01/ex07/ft_rev_int_tab.c
@@ -10,13 +10,48 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+    write(1, &c, 1);
+}
+
+void	ft_putnbr(int nb)
+{
+    if (nb < 0)
+    {
+        ft_putchar('-');
+        ft_putnbr(-nb);
+    }
+    else if (nb > 9)
+    {
+        ft_putnbr(nb / 10);
+        ft_putnbr(nb % 10);
+    }
+    else
+    {
+        ft_putchar(nb + '0');
+    }
+}
+
 void	ft_rev_int_tab(int *tab, int size)
 {
 	int	index;
-
-	index = 0;
-	while(index < size) {
-		
+	int	arr[size];
+	
+	while(index < size)
+	{
+		arr[0] = *tab;
+        ft_putnbr(1);
+		tab++;
 		index++;
 	}
+}
+
+int main() {
+	int numbers[] = {0,1};
+	int *tab = &numbers[0];
+	int size = 2;
+	ft_rev_int_tab(tab, size);
 }
