514c49c593ce83d07bdbbde71f08824e4dc26cf4
ibour <ianbour2016@gmail.com>
2024-07-14 13:40:02 +0200

    Finished C-02/ex02

---
+++ C-02/ex02/ft_str_is_alpha.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_str_is_alpha.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ibour <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/14 13:20:18 by ibour             #+#    #+#             */
+/*   Updated: 2024/07/14 13:20:20 by ibour            ###   ########.fr       */
+/*                                                                            */
+#include <stdio.h>
+
+/* ************************************************************************** */
+
+int ft_str_is_alpha(char* str)
+{
+    char    c;
+
+    if(*str == '\0')
+    {
+        return (1);
+    }
+
+    c = *str;
+    while(c != '\0')
+    {
+        c = *str;
+        if (!(c >= 'A' && c <= 'Z') && !(c >= 'a' && c <= 'z') && c != 0)
+        {
+            return (0);
+        }
+        str++;
+    }
+    return (1);
+}
+
+int main(int argc, char** argv)
+{
+    printf("Testing string: %s\n", argv[1]);
+    printf("Result: %d\n", ft_str_is_alpha(argv[1]));
+}
