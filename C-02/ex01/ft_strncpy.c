ba0a880a77242bb97ac22ce98c9cf46372934b8c
ibour <ianbour2016@gmail.com>
2024-07-15 10:24:08 +0200

    Fixed C-02/ex01 norminette

--- C-02/ex01/ft_strncpy.c
+++ C-02/ex01/ft_strncpy.c
@@ -8,13 +8,11 @@
 /*   Created: 2024/07/13 16:00:41 by ibour             #+#    #+#             */
 /*   Updated: 2024/07/13 16:20:22 by ibour            ###   ########.fr       */
 /*                                                                            */
-#include <stdio.h>
-
 /* ************************************************************************** */
 
 char	*ft_strncpy(char *dest, char *src, unsigned int n)
 {
-	unsigned int index;
+	unsigned int	index;
 
 	index = 0;
 	while (src[index] != '\0' && index < n)
