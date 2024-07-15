cd9a3495c939fee53b0a6e46b88870cac5be4615
IanToujou <ianbour2016@gmail.com>
2024-07-15 21:45:48 +0200

    ✨ Finished C-03/ex00

--- C-03/ex00/ft_strcmp.c
+++ C-03/ex00/ft_strcmp.c
@@ -10,14 +10,26 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
+
 int	ft_strcmp(char* s1, char* s2)
 {
-	char	c1;
-	char	c2;
-
+	while (*s1 != '\0' && (*s1 == *s2))
+	{
+		s1++;
+		s2++;
+	}
+	return (*(unsigned char*)s1 - *(unsigned char*)s2);
 }
 
 int	main(int argc, char** argv)
 {
-
+	if(argv[1] == (void *)0 || argv[2] == (void *)0)
+	{
+		printf("You need to input at least two strings!");
+		return 0;
+	}
+	printf("Input 1: %s", argv[1]);
+	printf("Input 2: %s", argv[2]);
+	printf("Result: %d", ft_strcmp(argv[1], argv[2]));
 }
