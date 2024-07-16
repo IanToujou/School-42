f96c25e86fe60fc040400e62abfec43f441b9929
ibour <ianbour2016@gmail.com>
2024-07-16 12:36:25 +0200

    C-03 - Fixed norminette

--- C-03/ex01/ft_strncmp.c
+++ C-03/ex01/ft_strncmp.c
@@ -13,11 +13,11 @@
 int	ft_strncmp(char *s1, char *s2, unsigned int n)
 {
 	unsigned int	i;
+
 	i = 0;
 	if (n == 0)
 		return (0);
 	while (s1[i] == s2[i] && (i < n - 1) && s1[i])
 		i++;
 	return (s1[i] - s2[i]);
-
 }
