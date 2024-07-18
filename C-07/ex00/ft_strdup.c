6156c1d1e658381e6f4eca7782b30738f12a74a8
ibour <ianbour2016@gmail.com>
2024-07-18 11:09:24 +0200

    Fixed C-06 ex00

--- C-07/ex00/ft_strdup.c
+++ C-07/ex00/ft_strdup.c
@@ -1,27 +1,43 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_print_program_name.c                            :+:      :+:    :+:   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ibour <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/07/18 07:27:03 by ibour             #+#    #+#             */
-/*   Updated: 2024/07/18 07:27:03 by ibour            ###   ########.fr       */
+/*   Created: 2024/07/18 08:24:10 by ibour             #+#    #+#             */
+/*   Updated: 2024/07/18 08:24:32 by ibour            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
+#include <stdlib.h>
 
-void	ft_putstr(char *str)
+int	ft_strlen(char *str)
 {
-	while (*str)
-		write(1, str++, 1);
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		i++;
+	}
+	return (i);
 }
 
-int	main(int argc, char **argv)
+char	*ft_strdup(char *src)
 {
-	if(argc < 1)
-        	return 0;
-	ft_putstr(argv[0]);
-	write(1, "\n", 1);
+	int		i;
+	char	*dest;
+
+	i = 0;
+	dest = (char *) malloc(ft_strlen(src) * sizeof(char) + 1);
+	if (dest == NULL)
+		return (0);
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
 }
--- C-07/ex01/ft_range.c
+++ C-07/ex01/ft_range.c
@@ -1,27 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_print_program_name.c                            :+:      :+:    :+:   */
+/*   ft_range.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ibour <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/07/18 07:27:03 by ibour             #+#    #+#             */
-/*   Updated: 2024/07/18 07:27:03 by ibour            ###   ########.fr       */
+/*   Created: 2024/07/18 09:41:00 by ibour             #+#    #+#             */
+/*   Updated: 2024/07/18 09:41:01 by ibour            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
+#include <stdlib.h>
 
-void	ft_putstr(char *str)
+int	*ft_range(int min, int max)
 {
-	while (*str)
-		write(1, str++, 1);
-}
-
-int	main(int argc, char **argv)
-{
-	if(argc < 1)
-        	return 0;
-	ft_putstr(argv[0]);
-	write(1, "\n", 1);
+	int	*array;
+	
+	if(max >= min)
+		return NULL;
+	
+	array = (int *) malloc(min);
 }
