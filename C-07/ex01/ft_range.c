b5c75d93b8b5964edf3b716ae62ae3e9757cde97
IanToujou <ianbour2016@gmail.com>
2024-07-20 22:49:22 +0200

    ✨ Added new files

--- C-07/ex01/ft_range.c
+++ C-07/ex01/ft_range.c
@@ -12,12 +12,22 @@
 
 #include <stdlib.h>
 
-int	*ft_range(int min, int max)
+int		*ft_range(int min, int max)
 {
-	int *array = NULL;
-	
-	if(max >= min)
-		return NULL;
-	
-	return array;
+	int	range;
+	int	index;
+	int	*buffer;
+
+	if (min >= max)
+		return (0);
+	range = max - min - 1;
+	if ((buffer = malloc(range * sizeof(int))) == NULL)
+		return (0);
+	index = 0;
+	while (index <= range)
+	{
+		buffer[index] = min + index;
+		index++;
+	}
+	return (buffer);
 }
