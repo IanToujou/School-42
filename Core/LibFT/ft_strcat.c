748a68357adc2d32ee6445a578d0832145527e34
IanToujou <ianbour2016@gmail.com>
2024-08-07 19:38:19 +0200

    ✨ Added ft_strcat.c

---
+++ Core/LibFT/ft_strcat.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   < ft_strcat.c >                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ibour <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/05 16:28:41 by ibour             #+#    #+#             */
+/*   Updated: 2024/08/05 16:28:41 by ibour            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/**
+ * Concatenates two string together, by adding the
+ * src string to the destination.
+ *
+ * @param dst The destination to copy to.
+ * @param src The source to copy from.
+ * @return A pointer to the first character of the
+ * destination.
+ */
+char	*ft_strcat(char *dst, char *src)
+{
+	char	*result;
+
+	result = dst;
+	while (*result != '\0')
+	{
+		result++;
+	}
+	while (*src != '\0')
+	{
+		*(unsigned char *) result = *(unsigned char *) src;
+		result++;
+		src++;
+	}
+	*result = '\0';
+	return (dst);
+}
--- Core/LibFT/libft.h
+++ Core/LibFT/libft.h
@@ -23,12 +23,16 @@ int		ft_isdigit(int c);
 
 size_t	ft_strlen(const char *s);
 char	*ft_strdup(char *src);
+char	*ft_strcpy(char *dst, const char *src);
+char	*ft_strncpy(char *dst, const char *src, unsigned int n);
+char	*ft_strcat(char *dst, char *src);
 
 void	*ft_memset(void *s, int c, size_t n);
 void	*ft_memcpy(void *dst, void *src, size_t n);
 void	*ft_memccpy(void *dest, void *src, int c, size_t n);
 void	*ft_memmove(void *dst, void *src, size_t s);
 void	*ft_memchr(const void *s, int c, size_t n);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
 void	ft_bzero(void *s, size_t n);
 
 typedef struct s_list
