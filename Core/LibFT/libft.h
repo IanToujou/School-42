f6b5ef3957dec7c3bb93879e5aaaea2fd77749cd
IanToujou <ianbour2016@gmail.com>
2024-08-17 21:01:11 +0200

    ✨ Added ft_strnequ.c

--- Core/LibFT/libft.h
+++ Core/LibFT/libft.h
@@ -60,6 +60,7 @@ void				ft_striteri(char *s, void (*f)(unsigned int, char *));
 char				*ft_strmap(char const *s, char (*f)(char));
 char				*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 int					ft_strequ(char const *s1, char const *s2);
+int					ft_strnequ(char const *s1, char const *s2, size_t n);
 
 typedef struct s_list
 {
