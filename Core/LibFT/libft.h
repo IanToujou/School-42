9213bee59bd4fe49f392b9dc9c6ba7a39aa94fad
ibour <ianbour2016@gmail.com>
2024-08-18 14:42:00 +0200

    Added ft_strsub.c

--- Core/LibFT/libft.h
+++ Core/LibFT/libft.h
@@ -61,6 +61,7 @@ char				*ft_strmap(char const *s, char (*f)(char));
 char				*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 int					ft_strequ(char const *s1, char const *s2);
 int					ft_strnequ(char const *s1, char const *s2, size_t n);
+char				*ft_strsub(char const *s, unsigned int start, size_t len);
 
 typedef struct s_list
 {
