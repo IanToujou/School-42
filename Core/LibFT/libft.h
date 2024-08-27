62785acb25d8d94db089ed96f90f4f0cfb0c22e2
IanToujou <ianbour2016@gmail.com>
2024-08-27 22:22:24 +0200

    ✨ Added ft_strsplit.c

--- Core/LibFT/libft.h
+++ Core/LibFT/libft.h
@@ -64,6 +64,7 @@ int					ft_strnequ(char const *s1, char const *s2, size_t n);
 char				*ft_strsub(char const *s, unsigned int start, size_t len);
 char				*ft_strjoin(char const *s1, char const *s2);
 char				*ft_strtrim(char const *s);
+char				**ft_strsplit(char const *s, char c);
 
 typedef struct s_list
 {
