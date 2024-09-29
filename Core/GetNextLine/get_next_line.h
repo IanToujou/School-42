84011bdfa924e8fd71f63fd601ae42f07c30b9a2
IanToujou <ianbour2016@gmail.com>
2024-09-29 19:38:42 +0200

    ✨ Added get next line utils

--- Core/GetNextLine/get_next_line.h
+++ Core/GetNextLine/get_next_line.h
@@ -24,6 +24,11 @@
 
 char	*get_next_line(int fd);
 
-__attribute__((unused)) void	*ft_calloc(size_t count, size_t size);
+size_t	ft_strlen(const char *s);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strchr(const char *s, int c);
+void	*ft_calloc(size_t count, size_t size);
+void	ft_bzero(void *s, size_t n);
+void	*ft_memset(void *s, int c, size_t n);
 
 #endif
--- Core/GetNextLine/get_next_line_utils.c
+++ Core/GetNextLine/get_next_line_utils.c
@@ -12,6 +12,58 @@
 
 #include "get_next_line.h"
 
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*result;
+	size_t	l1;
+	size_t	l2;
+	size_t	i;
+
+	if (s1 && s2)
+	{
+		l1 = ft_strlen(s1);
+		l2 = ft_strlen(s2);
+		result = (char *) malloc(sizeof(char) * (l1 + l2 + 1));
+		if (!result)
+			return (NULL);
+		i = -1;
+		while (s1[++i])
+			result[i] = s1[i];
+		i = -1;
+		while (s2[++i])
+		{
+			result[l1] = s2[i];
+			l1++;
+		}
+		result[l1] = '\0';
+		return (result);
+	}
+	return (NULL);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	char	*str;
+
+	str = (char *) s;
+	while (*str != c && *str != 0)
+		str++;
+	if (*str == c)
+		return (str);
+	else
+		return (NULL);
+}
+
 void	*ft_memset(void *s, int c, size_t n)
 {
     while (n--)
