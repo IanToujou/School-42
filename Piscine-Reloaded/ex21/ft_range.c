ca2e003403e95c384fc42c39459a895f2a002047
ibour <ianbour2016@gmail.com>
2024-10-14 19:27:14 +0200

    :sparkles: Added ex20 and ex21

---
+++ Piscine-Reloaded/ex21/ft_range.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_range.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ibour <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/17 14:15:07 by ibour             #+#    #+#             */
+/*   Updated: 2024/07/17 14:15:07 by ibour            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+int	*ft_range(int min, int max)
+{
+	int	*result;
+	int	i;
+
+	if (min >= max)
+		return (NULL);
+	result = (int *) malloc(sizeof(int) * (max - min - 1));
+	if (!result)
+		return (NULL);
+	i = 0;
+	while (min+i < max) {
+		result[i] = min+i;
+		i++;
+	}
+	return (result);
+}
