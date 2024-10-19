eef7363fa1bcb18c90de6fdf72755131e58ef33c
IanToujou <ianbour2016@gmail.com>
2024-10-19 20:07:15 +0200

    :truck: Moved ft_bzero.c to mandatory folder

--- Core/LibFT/src/base/ft_memmove.c
+++ Core/LibFT/src/base/ft_memmove.c
@@ -29,19 +29,19 @@ void	*ft_memmove(void *dst, void *src, size_t s)
 		return (NULL);
 	if (dst > src)
 	{
-		i = (int)s - 1;
+		i = (int) s - 1;
 		while (i >= 0)
 		{
-			*(char *)(dst + i) = *(char *)(src + i);
+			((char *)dst)[i] = ((char *)src)[i];
 			i--;
 		}
 	}
 	else
 	{
 		i = 0;
-		while (i < (int)s)
+		while (i < (int) s)
 		{
-			*(char *)(dst + i) = *(char *)(src + i);
+			((char *)dst)[i] = ((char *)src)[i];
 			i++;
 		}
 	}
