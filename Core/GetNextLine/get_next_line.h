808f96ed7baecaabc77c93b03cbc6d1a30383f46
IanToujou <ianbour2016@gmail.com>
2024-10-24 20:48:28 +0200

    :rewind: Restarted GetNextLine

--- Core/GetNextLine/get_next_line.h
+++ Core/GetNextLine/get_next_line.h
@@ -1,34 +1,5 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   < get_next_line.h >                                :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: ibour <marvin@42.fr>                       +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/08/05 16:28:41 by ibour             #+#    #+#             */
-/*   Updated: 2024/08/05 16:28:41 by ibour            ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #ifndef GET_NEXT_LINE_H
 
 # define GET_NEXT_LINE_H
-
-# ifndef BUFFER_SIZE
-# define BUFFER_SIZE 3
-# endif
-
-# include <unistd.h>
-# include <stdlib.h>
-# include <fcntl.h>
-
-char	*get_next_line(int fd);
-
-size_t	ft_strlen(const char *s);
-char	*ft_strjoin(char const *s1, char const *s2);
-char	*ft_strchr(const char *s, int c);
-void	*ft_calloc(size_t count, size_t size);
-void	ft_bzero(void *s, size_t n);
-void	*ft_memset(void *s, int c, size_t n);
 
 #endif
