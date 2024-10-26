02723316b90522e98af65ce5fcffab6e0320e8ab
ibour <ianbour2016@gmail.com>
2024-10-26 15:24:38 +0200

    :bug: Fixed ft_substr.c memory allocation

--- Core/LibFT/src/base/ft_substr.c
+++ Core/LibFT/src/base/ft_substr.c
@@ -1,6 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ibour <ibour@student.1337.ma>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/24 07:29:47 by ibour             #+#    #+#             */
+/*   Updated: 2024/10/26 15:16:11 by ibour            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
 /*   < ft_strsub.c >                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ibour <marvin@42.fr>                       +#+  +:+       +#+        */
@@ -23,18 +35,17 @@
  */
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-	char			*result;
-	unsigned int	i;
+	size_t	i;
+	size_t	length;
+	char	*result;
 
 	if (!s)
 		return (NULL);
-	if (start >= ft_strlen(s))
-	{
-		result = (char *)malloc(1);
-		if (result)
-			result[0] = '\0';
-		return (result);
-	}
+	length = ft_strlen((char *)s);
+	if (start >= length)
+		return (ft_strdup(""));
+	if (start + len > length)
+		len = length - start;
 	result = (char *) malloc(sizeof(char) * (len + 1));
 	if (result == NULL)
 		return (NULL);
