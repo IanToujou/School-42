93b021b563167344e37d80da69bf40e0306c4c2e
ibour <ianbour2016@gmail.com>
2024-11-04 10:48:04 +0100

    :bug: Fixed makefile

---
+++ Core/MiniTalk/lib/libft/src/custom/ft_strcmp.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   < ft_strcmp.c >                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ibour <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/05 16:28:41 by ibour             #+#    #+#             */
+/*   Updated: 2024/08/05 16:28:41 by ibour            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../include/libft.h"
+
+/**
+ * Compares two strings by going through the characters
+ * one by one, until a difference is found.
+ *
+ * @param s1 The first string to compare.
+ * @param s2 The second string to compare.
+ * @return The difference between the two characters,
+ * according to the ASCII table.
+ */
+int	ft_strcmp(const char *s1, const char *s2)
+{
+	size_t	i;
+
+	i = 0;
+	while (s1[i] && s2[i] && s1[i] == s2[i])
+		++i;
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+}
