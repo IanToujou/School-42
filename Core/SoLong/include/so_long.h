5ebd92e365c08f3f9bab0fda28fed91bd5f59bbe
ibour <ianbour2016@gmail.com>
2024-11-06 14:34:14 +0100

    :truck: Moved hooks

--- Core/SoLong/include/so_long.h
+++ Core/SoLong/include/so_long.h
@@ -6,7 +6,7 @@
 /*   By: ibour <support@toujoustudios.net>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/06 12:56:12 by ibour             #+#    #+#             */
-/*   Updated: 2024/11/06 13:06:00 by ibour            ###   ########.fr       */
+/*   Updated: 2024/11/06 13:18:28 by ibour            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,10 @@
 #define SO_LONG_H
 
 # include <stdlib.h>
+# include <X11/X.h>
+# include <X11/keysym.h>
 # include "../lib/mlx/mlx.h"
+# include "../lib/libft/include/libft.h"
 
 typedef struct s_mlx_data {
 	void	*data;
--- Core/SoLong/src/main/main.c
+++ Core/SoLong/src/main/main.c
@@ -6,22 +6,39 @@
 /*   By: ibour <support@toujoustudios.net>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/06 12:48:45 by ibour             #+#    #+#             */
-/*   Updated: 2024/11/06 13:03:32 by ibour            ###   ########.fr       */
+/*   Updated: 2024/11/06 13:23:26 by ibour            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/so_long.h"
 
+int	ft_hook_destroy(const t_mlx_data *mlx_data)
+{
+	mlx_destroy_window(mlx_data->data, mlx_data->window);
+	mlx_destroy_display(mlx_data->data);
+	free(mlx_data->data);
+	exit(0);
+	return (0);
+}
+
+int	ft_hook_keypress(const int keycode, const t_mlx_data *mlx_data)
+{
+	(void) mlx_data;
+	ft_printf("Pressed key: %d\n", keycode);
+	return (0);
+}
+
 int	main(void)
 {
-	void	*mlx;
-	void	*window;
+	t_mlx_data	mlx_data;
 
-	mlx = mlx_init();
-	if (!mlx)
+	mlx_data.data = mlx_init();
+	if (!mlx_data.data)
 		return (1);
-	window = mlx_new_window(mlx, 600, 10, "So Long");
-	if (!window)
-		return (free(mlx), 1);
-
+	mlx_data.window = mlx_new_window(mlx_data.data, 600, 500, "So Long");
+	if (!mlx_data.window)
+		return (free(mlx_data.data), 1);
+	mlx_hook(mlx_data.window, KeyRelease, KeyReleaseMask, ft_hook_keypress, &mlx_data);
+	mlx_hook(mlx_data.window, DestroyNotify, StructureNotifyMask, ft_hook_destroy, &mlx_data);
+	mlx_loop(mlx_data.data);
 }
