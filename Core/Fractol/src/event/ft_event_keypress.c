f189e6ed6bfdeac0d5339e58827c123926eb988b
ibour <ianbour2016@gmail.com>
2024-12-05 08:22:07 +0100

    :bug: Fixed key and close hooks

--- Core/Fractol/src/event/ft_event_keypress.c
+++ Core/Fractol/src/event/ft_event_keypress.c
@@ -6,7 +6,7 @@
 /*   By: ibour <support@toujoustudios.net>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/01 06:30:33 by ibour             #+#    #+#             */
-/*   Updated: 2024/12/01 14:33:22 by ibour            ###   ########.fr       */
+/*   Updated: 2024/12/03 07:39:17 by ibour            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ void	ft_event_keypress(int key, t_engine *engine)
 	else if (key >= KEY_LEFT && key <= KEY_UP)
 		ft_set_view(key, engine);
 	else if (key >= KEY_ONE && key <= KEY_FIVE)
-		change_fractal(key, engine);
+		ft_change_fractal(key, engine);
 	else if (key == KEY_L && engine->fractal.type == JULIA)
 		engine->fractal.julia_lock ^= 1;
 	else if (key == KEY_ZERO)
