43a395df2d309be31704544d0b3cda461c7dcf69
IanToujou <ianbour2016@gmail.com>
2025-01-02 12:22:51 +0100

    :sparkles: Added philosopher header file

---
+++ Core/Philosophers/philo/include/philosophers.h
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosophers.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ibour <support@toujoustudios.net>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/02 12:20:13 by ibour             #+#    #+#             */
+/*   Updated: 2025/01/02 12:22:10 by ibour            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILOSOPHERS_H
+
+# define PHILOSOPHERS_H
+
+# define TRUE 		1
+# define SUCCESS	1
+# define FALSE		0
+
+# include <pthread.h>
+# include <stdio.h>
+# include <limits.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <sys/time.h>
+
+typedef struct s_philosopher
+{
+	int					id;
+	int					last_meal;
+	int					is_eating;
+	int					time_to_die;
+	int					time_to_sleep;
+	int					time_to_eat;
+	int					number_of_meals;
+	pthread_t			thread_id;
+	struct s_general	*general;
+	pthread_mutex_t		*left_fork;
+	pthread_mutex_t		*right_fork;
+}	t_philosopher;
+
+typedef struct s_data
+{
+	int					number_of_philosophers;
+	int					time_to_die;
+	int					time_to_sleep;
+	int					time_to_eat;
+	int					number_of_meals;
+	int					starting_time;
+	int					philosopher_dead;
+	pthread_mutex_t		*fork_mutex;
+	pthread_mutex_t		mutex;
+	pthread_mutex_t		*left_fork;
+	pthread_mutex_t		*right_fork;
+	t_philo				*philosophers;
+}	t_data;
+
+#endif
--- Core/Philosophers/philo/src/main/main.c
+++ Core/Philosophers/philo/src/main/main.c
@@ -6,10 +6,14 @@
 /*   By: ibour <support@toujoustudios.net>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/24 13:10:11 by ibour             #+#    #+#             */
-/*   Updated: 2024/12/24 13:10:15 by ibour            ###   ########.fr       */
+/*   Updated: 2025/01/02 12:22:12 by ibour            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../../include/philosophers.h"
+
 int main() {
+	t_data	*data;
+
 
 }
