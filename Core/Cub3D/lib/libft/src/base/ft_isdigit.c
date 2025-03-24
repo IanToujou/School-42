669c24b33bcc6eae336413cc6e675c1293830990
ibour <ianbour2016@gmail.com>
2025-03-24 13:22:36 +0100

    Started Cub3D

---
+++ Core/Cub3D/lib/libft/src/base/ft_isdigit.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   < ft_isdigit.c >                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ibour <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/05 16:28:41 by ibour             #+#    #+#             */
+/*   Updated: 2024/08/05 16:28:41 by ibour            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../include/libft.h"
+
+/**
+ * Checks whether a character is a digit, from 0 to 9.
+ * It does this by comparing the ASCII values.
+ *
+ * @param c The character to check.
+ * @return 0 if the condition is false, and 1 on true.
+ */
+int	ft_isdigit(int c)
+{
+	return (c >= 48 && c <= 57);
+}
