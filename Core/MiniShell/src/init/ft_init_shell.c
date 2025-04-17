a98e8c399e500ef50cbaa20cd9e16ce289571cbd
IanToujou <ianbour2016@gmail.com>
2025-04-18 01:16:07 +0200

    Added new parser

--- Core/MiniShell/src/init/ft_init_shell.c
+++ Core/MiniShell/src/init/ft_init_shell.c
@@ -6,7 +6,7 @@
 /*   By: mwelfrin <mwelfrin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/15 09:28:30 by ibour             #+#    #+#             */
-/*   Updated: 2025/04/18 01:15:14 by ibour            ###   ########.fr       */
+/*   Updated: 2025/04/18 01:15:54 by ibour            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,30 @@
 
 static void	ft_run_shell(t_shell *shell, t_env_list *env_list)
 {
-	char	*prompt;
+	char	*prompt_pre;
+	char	*prompt_full;
 	char	*buffer;
 	char	*user;
 
-	user = ft_util_env_get(&env_list, "user");
-	prompt = ft_strjoin("kek", user);
-	buffer = readline(prompt);
+	user = ft_util_env_get(&env_list, "USER");
+	if (user)
+		prompt_pre = ft_strjoin(ft_util_banner_prompt_pre(), user);
+	else
+		prompt_pre = ft_strjoin(ft_util_banner_prompt_pre(), "nya");
+	if (prompt_pre == NULL)
+		ft_error_throw(ERROR_MALLOC);
+	prompt_full = ft_strjoin(prompt_pre, ft_util_banner_prompt_post());
+	buffer = readline(prompt_full);
 	if (!buffer) {
 		ft_putstr_fd("exit\n", STDOUT_FILENO);
 		shell->is_running = FALSE;
 		return ;
 	}
 	add_history(buffer);
-	ft_parse_input(shell, env_list, buffer, "nya");
+	ft_parse_input(shell, env_list, buffer, user);
 	free(buffer);
-	free(prompt);
+	free(prompt_full);
+	free(prompt_pre);
 }
 
 /**
--- Core/MiniShell/src/parse/ft_parse_input.c
+++ Core/MiniShell/src/parse/ft_parse_input.c
@@ -6,7 +6,7 @@
 /*   By: mwelfrin <mwelfrin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/15 10:29:13 by ibour             #+#    #+#             */
-/*   Updated: 2025/04/18 01:12:01 by ibour            ###   ########.fr       */
+/*   Updated: 2025/04/18 01:16:05 by ibour            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -70,6 +70,6 @@ t_bool	ft_parse_input(t_shell *shell, t_env_list *env_list, char *input, char *u
 		return ft_parse_exit(shell, result, STATUS_OK);
 	if (!ft_parse_handle(shell, env_list, result))
 		ft_error_throw(ERROR_MALLOC);
-	free(input);
+	//free(input);
 	return (TRUE);
 }
