be6d5a3e9e2992c54e180fc7740b3ecf5223f96c
Melisa Selin Welfringer <melisaselinmusellim@gmail.com>
2025-04-25 13:20:21 +0200

    Update ft_cmd_export.c

--- Core/MiniShell/src/cmd/export/ft_cmd_export.c
+++ Core/MiniShell/src/cmd/export/ft_cmd_export.c
@@ -12,57 +12,69 @@
 
 #include "../../../include/minishell.h"
 
-static void	ft_cmd_args(t_env_list **env_list, t_token *args, int *all_valid)
+static void	ft_export_error(char *arg)
 {
-	while (args)
-	{
-		if (!ft_is_valid_env_name(args->str))
-		{
-			ft_putstr_fd("minishell: export: `", STDERR_FILENO);
-			ft_putstr_fd(args->str, STDERR_FILENO);
-			ft_putstr_fd("': not a valid identifier\n", STDERR_FILENO);
-			*all_valid = 0;
-		}
-		else
-			ft_cmd_export_handle_env(env_list, args->str);
-		args = args->next;
-	}
-}
-
-void	ft_cmd_export(t_shell *shell, t_env_list **env_list, t_token *args)
-{
-	int	all_valid;
-
-	if (!args)
-	{
-		ft_util_env_print_exported(*env_list);
-		shell->exit_status = STATUS_OK;
-		return ;
-	}
-	all_valid = 1;
-	ft_cmd_args(env_list, args, &all_valid);
-	if (all_valid)
-		shell->exit_status = STATUS_OK;
-	else
-		shell->exit_status = STATUS_MAIN;
+	ft_putstr_fd("minishell: export: `", STDERR_FILENO);
+	ft_putstr_fd(arg, STDERR_FILENO);
+	ft_putstr_fd("': not a valid identifier\n", STDERR_FILENO);
 }
 
 void	ft_cmd_export_handle_env(t_env_list **env_list, char *str)
 {
-	char	*equal_sign;
+	char	*equal;
 	char	*key;
 
-	equal_sign = ft_strchr(str, '=');
-	if (!equal_sign)
+	equal = ft_strchr(str, '=');
+	if (!equal)
 	{
 		if (!ft_util_env_get(env_list, str))
 			ft_util_env_var_add(env_list, str);
+	}
+	else
+	{
+		key = ft_substr(str, 0, equal - str);
+		if (!key)
+			ft_error_throw(ERROR_MALLOC);
+		ft_util_env_var_remove(env_list, key);
+		ft_util_env_var_add(env_list, str);
+		free(key);
+	}
+}
+
+static int	ft_handle_export_arg(t_env_list **env, t_token *arg)
+{
+	if (!ft_is_valid_env_name(arg->str))
+	{
+		ft_export_error(arg->str);
+		return (1);
+	}
+	ft_cmd_export_handle_env(env, arg->str);
+	return (0);
+}
+
+static void	ft_process_export_args(t_shell *shell, t_env_list **env, t_token *args)
+{
+	int	has_error;
+
+	has_error = 0;
+	while (args)
+	{
+		has_error += ft_handle_export_arg(env, args);
+		args = args->next;
+	}
+	if (has_error)
+		shell->exit_status = EXIT_FAILURE;
+	else
+		shell->exit_status = EXIT_SUCCESS;
+}
+
+void	ft_cmd_export(t_shell *shell, t_env_list **env_list, t_token *args)
+{
+	if (!args)
+	{
+		ft_util_env_print_exported(*env_list);
+		shell->exit_status = EXIT_SUCCESS;
 		return ;
 	}
-	key = ft_substr(str, 0, equal_sign - str);
-	if (!key)
-		ft_error_throw(STATUS_MALLOC);
-	ft_util_env_var_remove(env_list, key);
-	ft_util_env_var_add(env_list, str);
-	free(key);
+	ft_process_export_args(shell, env_list, args);
 }
