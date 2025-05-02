33f00bf7cd830a5b388e29727e128cec9123719d
Melisa Selin Welfringer <melisaselinmusellim@gmail.com>
2025-05-02 16:52:17 +0200

    Update and rename ft_cmd_unset.c to unset update.c

--- Core/MiniShell/src/cmd/unset/unset update.c
+++ Core/MiniShell/src/cmd/unset/unset update.c
@@ -12,17 +12,47 @@
 
 #include "../../../include/minishell.h"
 
+static int	is_valid_env_var(char *str)
+{
+	int	i;
+
+	if (!str || str[0] == '=' || (!ft_isalpha(str[0]) && str[0] != '_'))
+		return (0);
+	i = 1;
+	while (str[i])
+	{
+		if (!ft_isalnum(str[i]) && str[i] != '_')
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+static void	unset_var(t_shell *shell, t_env_list **env_list, char *var)
+{
+	if (is_valid_env_var(var))
+		ft_util_env_var_remove(env_list, var);
+	else
+	{
+		ft_putstr_fd("minishell: unset: `", STDERR_FILENO);
+		ft_putstr_fd(var, STDERR_FILENO);
+		ft_putstr_fd("': not a valid identifier\n", STDERR_FILENO);
+		shell->exit_status = STATUS_WRONG_COMMAND;
+	}
+}
+
 void	ft_cmd_unset(t_shell *shell, t_env_list **env_list, t_token *args)
 {
 	if (!args)
 	{
-		shell->exit_status = EXIT_SUCCESS;
+		ft_putstr_fd("minishell: unset: not enough arguments\n", STDERR_FILENO);
+		shell->exit_status = STATUS_WRONG_COMMAND;
 		return ;
 	}
+	shell->exit_status = STATUS_OK;
 	while (args)
 	{
-		ft_util_env_var_remove(env_list, args->str);
+		unset_var(shell, env_list, args->str);
 		args = args->next;
 	}
-	shell->exit_status = EXIT_SUCCESS;
 }
