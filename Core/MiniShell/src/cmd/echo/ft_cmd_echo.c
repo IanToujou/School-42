e89f308a98b67a64a1d7e7e6c608445ff0203761
Melisa Selin Welfringer <mwelfrin@c1r4s5.42luxembourg.lu>
2025-05-28 08:26:05 +0200

    echo 0 issue is fixed

--- Core/MiniShell/src/cmd/echo/ft_cmd_echo.c
+++ Core/MiniShell/src/cmd/echo/ft_cmd_echo.c
@@ -12,30 +12,59 @@
 
 #include "../../../include/minishell.h"
 
-void	ft_cmd_echo(t_shell *shell, t_token *token)
+static t_token	*ft_cmd_echo_handle_n_flags(t_token *token, t_bool *newline)
 {
-	t_bool	newline;
-	int		i;
+	int	i;
 
-	newline = TRUE;
+	*newline = TRUE;
 	while (token && token->type == TOKEN_ARG && token->str[0] == '-')
 	{
 		i = 1;
 		while (token->str[i] == 'n')
 			i++;
 		if (i > 1 && token->str[i] == '\0')
+		{
+			*newline = FALSE;
 			token = token->next;
+		}
 		else
 			break ;
-		newline = FALSE;
 	}
+	return (token);
+}
+
+static void	ft_cmd_echo_print_args(t_shell *shell, t_token *token)
+{
+	t_parse	parse;
+	char	*expanded_str;
+
 	while (token && token->type == TOKEN_ARG)
 	{
-		ft_putstr_fd(token->str, STDOUT_FILENO);
+		parse = ft_init_parse_data();
+		if (ft_parse_dollar_search(token->str))
+		{
+			expanded_str = ft_parse_dollar(shell->env_list, &parse,
+					token->str, shell);
+			if (expanded_str)
+			{
+				ft_putstr_fd(expanded_str, STDOUT_FILENO);
+				free(expanded_str);
+			}
+		}
+		else
+			ft_putstr_fd(token->str, STDOUT_FILENO);
 		if (token->next && token->next->type == TOKEN_ARG)
 			ft_putchar_fd(' ', STDOUT_FILENO);
 		token = token->next;
 	}
+}
+
+void	ft_cmd_echo(t_shell *shell, t_token *token)
+{
+	t_bool	newline;
+
+	token = ft_cmd_echo_handle_n_flags(token, &newline);
+	ft_cmd_echo_print_args(shell, token);
 	if (newline)
 		ft_putchar_fd('\n', STDOUT_FILENO);
 	shell->exit_status = STATUS_OK;
