8dac1373e093fcb664b52433145f2a84d0c3e7b1
ibour <ianbour2016@gmail.com>
2025-06-20 10:45:04 +0200

    Added cylinder parsing

--- Core/MiniRT/src/parse/parse_cylinder.c
+++ Core/MiniRT/src/parse/parse_cylinder.c
@@ -6,13 +6,24 @@
 /*   By: ibour <support@toujoustudios.net>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/19 14:45:22 by ibour             #+#    #+#             */
-/*   Updated: 2025/06/20 09:07:10 by ibour            ###   ########.fr       */
+/*   Updated: 2025/06/20 10:43:25 by ibour            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minirt.h"
 
 void	parse_cylinder(t_scene *scene, char **str) {
-	(void)scene;
-	(void)str;
+	t_cylinder	*cylinder;
+
+	if (util_array_count((void **)str) != 6)
+		error_throw(ERROR_PARSE_CYLINDER);
+	cylinder = malloc(sizeof(t_cylinder));
+	if (!cylinder)
+		error_throw(ERROR_MALLOC);
+	cylinder->point = util_vector_parse(str[1]);
+	cylinder->axis = util_vector_parse(str[2]);
+	cylinder->radius = util_num_parse(str[3]) / 2;
+	cylinder->height = util_num_parse(str[4]);
+	cylinder->color = util_color_parse(str[5]);
+	add_object_to_scene(scene, OBJ_CYLINDER, cylinder);
 }
