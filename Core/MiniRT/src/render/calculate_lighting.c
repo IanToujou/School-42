14e32cf662c9d7ca5cab8420eff6442f89becd08
ibour <ianbour2016@gmail.com>
2025-06-20 13:41:19 +0200

    Fixed ambient light

--- Core/MiniRT/src/render/calculate_lighting.c
+++ Core/MiniRT/src/render/calculate_lighting.c
@@ -6,7 +6,7 @@
 /*   By: mpoesy <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/10 17:01:49 by mpoesy            #+#    #+#             */
-/*   Updated: 2025/06/20 13:20:20 by ibour            ###   ########.fr       */
+/*   Updated: 2025/06/20 13:37:53 by ibour            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,16 +40,16 @@ static int	is_shadow(t_scene *scene, t_vec3 hit_point, t_vec3 light_pos)
 static t_color calculate_ambient(const t_hit_info *hit, t_scene *scene)
 {
 	t_color ambient;
-	t_vec3 light_dir;
-	double diff;
-	t_color diffuse;
 
+	// Ambient light doesn't have a position or direction - it's uniform
 	ambient = color_scale(hit->color, scene->ambient.ratio);
-	light_dir = vec_normalize(vec_sub(scene->ambient.position, hit->point));
-	diff = fmax(vec_dot(hit->normal, light_dir), 0.0);
-	diffuse = color_scale(hit->color, diff * scene->ambient.ratio);
-	return (add_color(ambient, diffuse));
+
+	// Apply ambient light color
+	ambient = util_color_multiply(ambient, scene->ambient.color);
+
+	return (ambient);
 }
+
 
 t_color calculate_lighting(const t_hit_info *hit, t_scene *scene)
 {
