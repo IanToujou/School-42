35c8c46d5835cebf9ec54f0d5f61f96ab7f2ed28
ibour <ianbour2016@gmail.com>
2025-07-07 19:14:03 +0200

    Finished ex03

---
+++ Core/CPP/Module-02/ex03/src/bsp.cpp
@@ -0,0 +1,27 @@
+#include "../include/Point.h"
+
+static float calculate_area(const Point a, const Point b, const Point c)
+{
+	const float area = (( a.getX().toFloat() * ( b.getY().toFloat() - c.getY().toFloat()))
+			+ ( b.getX().toFloat() * ( c.getY().toFloat() - a.getY().toFloat()))
+			+ ( c.getX().toFloat() * ( a.getY().toFloat() - b.getY().toFloat()))) / 2;
+	if (area >= 0)
+		return (area);
+	return (area * -1);
+}
+
+bool		bsp(const Point a, const Point b, const Point c, const Point point) {
+
+	float d0, d1, d2, d3;
+	bool result = false;
+
+	d0 = calculate_area(a, b, c);
+	d1 = calculate_area(point, a, b);
+	d2 = calculate_area(point, b, c);
+	d3 = calculate_area(point, a, c);
+	if (d1 == 0 || d2 == 0 || d3 == 0)
+		result = false;
+	else if (d1 + d2 + d3 == d0)
+		result = true;
+	return result;
+}
--- Core/CPP/Module-02/ex03/src/main.cpp
+++ Core/CPP/Module-02/ex03/src/main.cpp
@@ -1,14 +1,39 @@
 #include "../include/Fixed.h"
+#include "../include/Point.h"
+
+bool bsp(Point a, Point b, Point c, Point point);
 
 int main() {
-	Fixed a;
-	Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
-	std::cout << a << std::endl;
-	std::cout << ++a << std::endl;
-	std::cout << a << std::endl;
-	std::cout << a++ << std::endl;
-	std::cout << a << std::endl;
-	std::cout << b << std::endl;
-	std::cout << Fixed::max( a, b ) << std::endl;
-	return 0;
+	{
+		const Point a;
+		const Point b(10, 0);
+		const Point c(0, 10);
+		const Point point(1, 1);
+
+		std::cout << "The point( x= " << point.getX() << "\t\ty= " << point.getY() << "\t) is inside the triangle\n" <<
+		"\ta( x= " << a.getX() << "\t\ty= " << a.getY() << "\t)\n" <<
+		"\tb( x= " << b.getX() << "\ty= " << b.getY() << "\t)\n" <<
+		"\tc( x= " << c.getX() << "\t\ty= " << c.getY() << "\t)\n" << std::endl;
+		if (bsp(a, b, c, point) == true)
+			std::cout << "\033[32mTRUE\033[0m" << std::endl;
+		else
+			std::cout << "\033[31mFALSE\033[0m" << std::endl;
+	}
+	std::cout << "\n---------------------------------------------------------------------\n" << std::endl;
+	{
+		Point a(-1.5, -1.5);
+		Point b(2.5, 2.5);
+		Point c(-1, -2);
+		Point point(8.5, -9);
+
+		std::cout << "The point( x= " << point.getX() << "\ty= " << point.getY() << "\t) is inside the triangle\n" <<
+		"\ta( x= " << a.getX() << "\ty= " << a.getY() << "\t)\n" <<
+		"\tb( x= " << b.getX() << "\ty= " << b.getY() << "\t)\n" <<
+		"\tc( x= " << c.getX() << "\ty= " << c.getY() << "\t)\n" << std::endl;
+		if (bsp(a, b, c, point) == true)
+			std::cout << "\033[32mTRUE\033[0m" << std::endl;
+		else
+			std::cout << "\033[31mFALSE\033[0m" << std::endl;
+	}
+	return (0);
 }
