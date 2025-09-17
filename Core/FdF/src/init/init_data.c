c27ad38e1e19f0d781946399892320b955ec0dbc
IanToujou <ianbour2016@gmail.com>
2025-09-18 00:58:48 +0200

    Added debug method

--- Core/FdF/src/init/init_data.c
+++ Core/FdF/src/init/init_data.c
@@ -6,7 +6,7 @@
 /*   By: ibour <support@toujoustudios.net>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/17 09:40:10 by ibour             #+#    #+#             */
-/*   Updated: 2025/09/17 19:25:33 by ibour            ###   ########.fr       */
+/*   Updated: 2025/09/18 00:58:43 by ibour            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void	init_data(t_data *data)
 {
 	data->map.altitude_ratio = 1;
 	data->map.projection = ISOMETRIC;
-	data->map.color = RAINBOW;
+	data->map.color = ALTITUDE;
 	data->mlx_ptr = NULL;
 	data->win_ptr = NULL;
 	data->img_ptr = NULL;
