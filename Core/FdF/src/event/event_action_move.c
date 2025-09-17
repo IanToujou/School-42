952f844179e98306da05dbb25fa31c2dfc061867
IanToujou <ianbour2016@gmail.com>
2025-09-18 00:55:05 +0200

    Added debug method

--- Core/FdF/src/event/event_action_move.c
+++ Core/FdF/src/event/event_action_move.c
@@ -6,7 +6,7 @@
 /*   By: ibour <support@toujoustudios.net>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/17 18:47:03 by ibour             #+#    #+#             */
-/*   Updated: 2025/09/18 00:52:03 by ibour            ###   ########.fr       */
+/*   Updated: 2025/09/18 00:55:03 by ibour            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,7 +34,7 @@ void	event_action_move(const int k, t_data *data)
 		data->map.y_offset -= OFFSET_INCR;
 	if (k == KEY_W)
 		data->map.y_offset += OFFSET_INCR;
-	if (!gfx_project(data))
+	/*if (!gfx_project(data))
 		error_throw(ERROR_RENDER_PROJECTION);
-	util_place(data);
+	util_place(data);*/
 }
