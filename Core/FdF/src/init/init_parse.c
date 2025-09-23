6ae8ce7e81fe5a6482553d92628dc8fc0b2e675f
ibour <ianbour2016@gmail.com>
2025-09-23 13:13:12 +0200

    Fixed leak

--- Core/FdF/src/init/init_parse.c
+++ Core/FdF/src/init/init_parse.c
@@ -6,7 +6,7 @@
 /*   By: ibour <support@toujoustudios.net>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/17 09:46:36 by ibour             #+#    #+#             */
-/*   Updated: 2025/07/22 08:53:07 by ibour            ###   ########.fr       */
+/*   Updated: 2025/09/23 13:13:00 by ibour            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,14 +18,19 @@ void	init_parse(t_data *data, const char *file)
 
 	fd = open(file, O_RDONLY);
 	if (fd == -1)
+	{
+		free(data);
 		error_throw(ERROR_FILE_INVALID);
+		return ;
+	}
 	data->map.depth = util_map_parse_depth(fd);
 	data->map.width = util_map_parse_width(file);
 	close(fd);
 	if (data->map.depth <= 0 || data->map.width <= 0)
 	{
-		exit_data(data);
+		free(data);
 		error_throw(ERROR_FILE_INVALID);
+		return ;
 	}
 	init_map_array(data);
 	init_map_read(data, file);
