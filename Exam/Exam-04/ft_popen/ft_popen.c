d7df1efd3930e982090bc0916ecedba0804f6948
IanToujou <ianbour2016@gmail.com>
2025-10-20 02:16:48 +0200

    Added debug method

--- Exam/Exam-04/ft_popen/ft_popen.c
+++ Exam/Exam-04/ft_popen/ft_popen.c
@@ -5,24 +5,23 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: ibour <support@toujoustudios.net>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/09/09 10:59:58 by ibour             #+#    #+#             */
-/*   Updated: 2025/10/16 11:55:48 by ibour            ###   ########.fr       */
+/*   Created: 2025/10/19 05:40:58 by ibour             #+#    #+#             */
+/*   Updated: 2025/10/19 05:50:57 by ibour            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
+#include <io.h>
+#include <process.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <sys/wait.h>
+#include <sys/types.h>
 
-void close_fd(const int *fd1, const int *fd2) {
+void ft_close(const int *fd1, const int *fd2) {
 	if (*fd1 != -1) close(*fd1);
 	if (*fd2 != -1) close(*fd2);
 }
 
-int	ft_popen(const char *file, char *const argv[], char type) {
+int ft_popen(const char *file, char *const argv[], char type) {
 
-	// Input checks
 	if (!file || !argv)
 		return -1;
 	if (type != 'r' && type != 'w')
@@ -30,69 +29,45 @@ int	ft_popen(const char *file, char *const argv[], char type) {
 
 	int fd[2] = {-1, -1};
 
-	// Pipe and fork
 	if (pipe(fd) < 0) {
-		close_fd(&fd[0], &fd[1]);
+		ft_close(&fd[0], &fd[1]);
 		return -1;
 	}
 
 	pid_t pid = fork();
-
 	if (pid < 0)
 		return -1;
 
 	if (type == 'r') {
 
 		if (pid == 0) {
-			if (dup2(fd[1], 1) != -1) { // important
-				close_fd(&fd[0], &fd[1]);
-				exit(-1);
+			if (dup2(fd[1], 1) != -1) {
+				ft_close(&fd[0], &fd[1]);
+				exit(1);
 			}
 			execvp(file, argv);
-			close_fd(&fd[0], &fd[1]);
+			ft_close(&fd[0], &fd[1]);
 			exit(-1);
 		}
+
 		close(fd[1]);
 		return fd[0];
 
 	} else {
 
 		if (pid == 0) {
-			if (dup2(fd[0], 0) != 0) { // same
-				close_fd(&fd[0], &fd[1]);
+			if (dup2(fd[0], 0) != 0) {
+				ft_close(&fd[0], &fd[1]);
 				exit(-1);
 			}
 			execvp(file, argv);
-			close_fd(&fd[0], &fd[1]);
+			ft_close(&fd[0], &fd[1]);
 			exit(-1);
 		}
+
 		close(fd[0]);
 		return fd[1];
 
 	}
 
-}
-
-int main() {
-	char *const av[] = {"ls", "-la", NULL};
-	int fd = ft_popen("ls", av, 'r');
-
-	if (fd == -1) {
-		printf("ft_popen failed\n");
-		return 1;
-	}
-
-	// Read the output from the command
-	char buffer[1024];
-	ssize_t bytes_read;
-
-	while ((bytes_read = read(fd, buffer, sizeof(buffer) - 1)) > 0) {
-		buffer[bytes_read] = '\0';
-		printf("%s", buffer);
-	}
-
-	close(fd);  // Close the file descriptor
-	wait(NULL); // Wait for child process to prevent zombie
-
-	return 0;
 }
