eddffbae602928bf0b0468bfa698c8ca6d84d59c
ibour <ianbour2016@gmail.com>
2025-11-01 20:43:23 +0100

    Fixed popen

--- Exam/Exam-04/ft_popen/ft_popen_solution.c
+++ Exam/Exam-04/ft_popen/ft_popen_solution.c
@@ -6,7 +6,7 @@
 /*   By: ibour <support@toujoustudios.net>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/09 10:59:58 by ibour             #+#    #+#             */
-/*   Updated: 2025/10/19 05:53:11 by ibour            ###   ########.fr       */
+/*   Updated: 2025/11/01 20:41:20 by ibour            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,39 +14,34 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-void ft_close(const int *fd1, const int *fd2) {
-	if (*fd1 != -1) close(*fd1);
-	if (*fd2 != -1) close(*fd2);
-}
-
 int ft_popen(const char *file, char *const argv[], char type) {
 
-	if (!file || !argv)
-		return -1;
-	if (type != 'r' && type != 'w')
-		return -1;
+	if (!file || !argv || !(type == 'r' || type == 'w'))
+		return (-1);
 
-	int fd[2] = {-1, -1};
+	int fd[2];
+	if (pipe(fd) == -1)
+		return (-1);
 
-	if (pipe(fd) < 0) {
-		ft_close(&fd[0], &fd[1]);
-		return -1;
+	const pid_t pid = fork();
+	if (pid == -1) {
+		close(fd[0]);
+		close(fd[1]);
+		return (-1);
 	}
-
-	pid_t pid = fork();
-	if (pid < 0)
-		return -1;
 
 	if (type == 'r') {
 
 		if (pid == 0) {
-			if (dup2(fd[1], 1) != -1) {
-				ft_close(&fd[0], &fd[1]);
-				exit(-1);
-			}
+
+			if (dup2(fd[1], 1) == -1)
+				exit(1);
+
+			close(fd[0]);
+			close(fd[1]);
 			execvp(file, argv);
-			ft_close(&fd[0], &fd[1]);
-			exit(-1);
+			exit(1);
+
 		}
 
 		close(fd[1]);
@@ -55,13 +50,15 @@ int ft_popen(const char *file, char *const argv[], char type) {
 	} else {
 
 		if (pid == 0) {
-			if (dup2(fd[0], 0) != 0) {
-				ft_close(&fd[0], &fd[1]);
-				exit(-1);
-			}
+
+			if (dup2(fd[0], 0) == -1)
+				exit(1);
+
+			close(fd[0]);
+			close(fd[1]);
 			execvp(file, argv);
-			ft_close(&fd[0], &fd[1]);
-			exit(-1);
+			exit(1);
+
 		}
 
 		close(fd[0]);
